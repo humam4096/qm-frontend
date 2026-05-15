7176626a5376701d53fd72b49ba6cdb4731f907f
BASIM THANI <mjnoonha90@gmail.com>
2026-05-15 15:48:23 +0300

    form builder update so question initially start with single_Select, two yes-no options

--- src/modules/complaints/components/ComplaintFormDialog.tsx
+++ src/modules/complaints/components/ComplaintFormDialog.tsx
@@ -156,7 +156,7 @@ export const ComplaintFormDialog: React.FC<Props> = ({
       footer
       contentClassName="md:max-w-2xl "
     >
-      <form onSubmit={handleSubmit(onSubmit)} className="max-h-[450px] overflow-y-scroll grid grid-cols-1 gap-4 sm:grid-cols-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="max-h-[450px] md:max-h-full overflow-y-scroll grid grid-cols-1 gap-4 sm:grid-cols-2">
         {/* Kitchen Selection */}
         <div className="space-y-2">
           <Label htmlFor="kitchen_id">{t("complaints.kitchen")}</Label>
